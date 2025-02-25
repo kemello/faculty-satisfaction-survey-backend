ba87d986d748fcd935bce522af77275ea2cb5079
kemelov <kemelovramazan@gmail.com>
2025-02-25 22:37:31 +0600

    refactor(student): rearrange fields in StudentInfo record
    - Move gender field to the beginning of the record
    - Swap the positions of studyMode and gender fields

--- src/main/java/kg/adam/faculty_satisfaction_survey/student/domain/model/StudentInfo.java
+++ src/main/java/kg/adam/faculty_satisfaction_survey/student/domain/model/StudentInfo.java
@@ -7,9 +7,9 @@ import kg.adam.faculty_satisfaction_survey.common.enums.Gender;
 import kg.adam.faculty_satisfaction_survey.common.enums.StudyMode;
 
 public record StudentInfo(
+        Gender gender,
         AcademicYear academicYear,
         Faculty faculty,
-        StudyMode studyMode,
-        Gender gender
+        StudyMode studyMode
 ) {
 }
